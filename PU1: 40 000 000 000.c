6e7709058ed7f56a544d42ac7b4c43568adfcb39
Kasparssen <90375574+Kasparssen@users.noreply.github.com>
2021-10-03 22:11:40 +0300

    Pirmais programmēšanas uzdevums
    Uzrakstiet kodu dialogam ar lietotāju. Lietotājam ir jāievada divi skaitļi - 400 000 (četri simti tūkstoši) un 100 000 (simts tūkstoši), programmai ir jāattēlo reizināšanas operācijas rezultāts - 40 000 000 000 (četrdesmit miljardi).

---
+++ PU1: 40 000 000 000.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+long pirmais;                                   
+long otrais;                                    
+printf("Ievadiet pirmo skaitli (400000): ");    //Iekavās doti prasītie skaitļi, jo kods nestrādā ar vērtībām, kas nav vesali skaitļi.
+scanf("%li", &pirmais);                         
+printf("Ievadiet otro skaitli (100000): ");
+scanf("%li", &otrais);                          
+printf("Reizinajums : %li \n", pirmais * otrais);
+return 0;
+}
